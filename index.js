3a2376e7555bc1ccb523783bfba122c00c92640e
viniciospereirarodrigues@gmail.com <viniciospereirarodrigues@gmail.com>
2022-07-31 13:06:52 -0300

    ajustes finais

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ let body = document.querySelector("body");
 let img = document.querySelector(".img");
 let p = document.querySelector("p");
 
-let data = new Date().getHours();
-console.log(data);
+let hora = new Date().getHours();
+console.log(hora);
 
-if (data >= 18 && data <= 5) {
-  body.style.background = "black";
-  img.src = "./img/noite.jpg";
-  console.log("noite");
-} else if (data > 5 && data < 12) {
-  console.log("manhã");
-  img.src = "./img/manha.jpg";
+if (hora >= 5 && hora < 12) {
   body.style.background = "blue";
-} else {
-  console.log("tarde");
+  img.src = "./img/manha.jpg";
+  p.innerText = `Bom dia, são ${hora} horas, e ainda esta os pela manhã`;
+} else if (hora >= 12 && hora < 18) {
   body.style.background = "orange";
   img.src = "./img/tarde.jpg";
-  p.innerText = `Boa tarde, são ${data} horas.`
+  p.innerText = `Boa tarde, são ${hora} horas, e já estamos na parte da tarde.`;
+} else {
+  body.style.background = "black";
+  img.src = "./img/noite.jpg";
+  p.innerText = `Boa noite, já são ${hora} horas, e logo iremos dormir.`;
 }
